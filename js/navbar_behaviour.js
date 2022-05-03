aa63a6733b3eeed98b7c6b22b863a02d42096349
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-05-03 17:35:55 +0000

    fix: minor tweaks

--- js/navbar_behaviour.js
+++ js/navbar_behaviour.js
@@ -1,29 +1,38 @@
+function applyStyle(apply,elt){
+    if (apply){
+        elt.style.backgroundColor = '#1c1b2299';
+        elt.style.backdropFilter = 'blur(10px)';
+        elt.style.transition = 'background .1s ease-in';
+    }else{
+        elt.style.backgroundColor = '#1c1b2200';
+        elt.style.transition = 'background .1s ease-out';
+        elt.style.backdropFilter = 'none';
+    }
+}
+let header = document.querySelector('header');
 document.body.onscroll = () => {
-    let header = document.querySelector('header');
     let toUp = document.querySelector('[href="#anchor"]');
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
-        header.style.backgroundColor = '#1c1b2299';
-        header.style.backdropFilter = 'blur(10px)';
-        header.style.transition = 'background .1s ease-in';
+        applyStyle(true, header);
         toUp.style.display = 'inline';
     }else{
-        header.style.backgroundColor = '#1c1b2200';
-        header.style.transition = 'background .1s ease-out';
+        applyStyle(false, header);
         toUp.style.display = 'none';
-        header.style.backdropFilter = 'none';
     }
 }
 let mobileNav = document.querySelector('#nav-mobile');
 let showNav = document.querySelector('#showNav');
 let hideNav = document.querySelector('#hideNav');
 showNav.onclick = () => {
-    mobileNav.parentElement.style.inset = '.4rem .3rem .4rem .4rem';
+    header.style.inset = '.4rem .3rem .4rem .4rem';
     hideNav.style.display = 'inline';
     showNav.style.display = 'none';
+    applyStyle(true, header);
 }
 
 hideNav.onclick = () =>{
     hideNav.parentElement.style.inset = '.4rem .3rem auto .4rem'
     showNav.style.display = 'inline';
     hideNav.style.display = 'none';
+    applyStyle(false, header);
 }
